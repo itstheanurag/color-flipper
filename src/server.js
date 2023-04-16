179e54556c0608a3c07c792b32113565a037161c
Gaurav <gauravanurag36@gmail.com>
2023-04-16 16:43:41 +0530

    feat: image upload with multer success

--- src/server.js
+++ src/server.js
@@ -1,41 +1,61 @@
 const express = require("express");
 const path = require("path");
 const config = require("../config");
-// const multer = require('multer');
+const multer = require("multer");
+const sharp = require("sharp");
+const jimp = require("jimp");
 
-// const sharp = require('sharp');
-const routes = require('./routes')
 const app = express();
 const APP_PORT = config.development.port;
 // Set the public folder as the static directory
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// const upload = multer({
-//     limits: {
-//         fileSize: 1000000
-//     },
-//     fileFilter(req, file, cb) {
-//         if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-//             return cb(new Error('Please upload a valid image file'))
-//         }
-//         cb(undefined, true)
-//     }
-// })
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("Please upload a valid image file"));
+    }
+    cb(undefined, true);
+  },
+});
 
-// app.post('/image', upload.single('upload'), async (req, res) => {
-//     try {
-//          await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toFile(__dirname + `public/images/${req.file.originalname}`)
-//          res.status(201).send('Image uploaded succesfully')
-//     } catch (error) {
-//         console.log(error)
-//         res.status(400).send(error)
-//     }
-// })
+app.post("/image", upload.single("image"), async (req, res) => {
+  try {
+    await sharp(req.file.buffer)
+      .png()
+      .toFile(__dirname + `/images/${req.file.originalname}`);
 
-app.use("/image", routes)
+    const imagePath = `/images/${req.file.originalname}`;
+
+    jimp.read(`src/${imagePath}`, (err, image) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      image
+        .background(0xffffffff)
+        .quality(100)
+        .write(`${req.file.originalname}`, (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log("Background removed successfully.");
+        });
+    });
+    // res.status(201).send("Image uploaded succesfully");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
 
 // Listen on port 3000
 app.listen(APP_PORT, () => {
-  console.log("Server started on port: ", APP_PORT);
+  console.log("Server started on port:", APP_PORT);
 });
